a93a13b62037b9a134b02264071ac760613b518e
Albert FreemanS <simplegadget512@gmail.com>
2017-09-20 12:39:41 -0500

    Initial commit of code/assets/etc.

---
+++ Assets/_Scripts/TrackGameObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TrackGameObject : MonoBehaviour {
+
+    public Transform target;
+
+	void Update () {
+        transform.LookAt(target);
+	}
+}
